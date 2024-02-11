617b41a582e20ad72c0dcce145b1ec33874ab9be
aldanaeguiluz <aldanaeguiluz@gmail.com>
2024-02-11 17:06:48 -0500

    endpoints

--- proyectos/urls.py
+++ proyectos/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import verEquiposEndpoint
+from .views import verEquiposEndpoint, verEquiposPathParametersEndpoint, loginEndpoint
 
 urlpatterns=[
-    path("ver-equipos", verEquiposEndpoint)
+    path("ver-equipos", verEquiposEndpoint),
+    path("ver-equipos-path/<str:filtro>", verEquiposPathParametersEndpoint),
+    path("login/<str:username>/<str:password>", loginEndpoint)
 ]
--- proyectos/views.py
+++ proyectos/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
+
+usuarios="""
+[
+    {
+        "username" : "usuario1",
+        "password" : "123"
+    },
+    {
+        "username" : "usuario2",
+        "password" : "abc"
+    },
+    {
+        "username" : "usuario3",
+        "password" : "aaa"
+    }
+]
+"""
 
 equipos= """
 [
@@ -33,4 +51,53 @@ equipos= """
 """
 
 def verEquiposEndpoint(request):
+    if request.method== "GET":
+        nombreFiltro=request.GET.get("nombre")
+
+        ##def filtro(equipo):
+        ##    print(equipo["nombre"])
+        ##    return equipo["nombre"].lower()==nombreFiltro
+
+        listaEquipos= json.loads(equipos)
+        ##listaEquiposFiltrada=list(filter(filtro, listaEquipos))
+        listaEquiposFiltrada=list(
+            filter(
+                lambda x: x["nombre"].lower()==nombreFiltro, 
+                listaEquipos))
+        return HttpResponse(json.dumps(listaEquiposFiltrada))
     return HttpResponse(equipos)
+
+def verEquiposPathParametersEndpoint(request, filtro):
+    if request.method== "GET":
+        nombreFiltro=filtro
+
+        listaEquipos= json.loads(equipos)
+        listaEquiposFiltrada=list(
+            filter(
+                lambda x: x["nombre"].lower()==nombreFiltro, 
+                listaEquipos))
+        return HttpResponse(json.dumps(listaEquiposFiltrada))
+    return HttpResponse(equipos)
+
+#Path: /proyectos/login GET
+def loginEndpoint(request, username, password):
+    if request.method == "GET":
+        #Peticion GET
+        listaUsuarios=json.loads(usuarios)
+        listaUsuariosFiltrada=list(
+            filter(
+                lambda x:x["username"]==username and x["password"]==password,
+                listaUsuarios
+            )
+        )
+
+        if len(listaUsuariosFiltrada)>0:
+            respuesta={
+                "msg":""
+            }
+            return HttpResponse(json.dumps(respuesta))
+        else:
+            respuesta={
+                "msg": "Error en el login"
+            }
+            return HttpResponse(json.dumps(respuesta))
